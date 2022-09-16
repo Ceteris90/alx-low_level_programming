37e032386cc6e226a0d251fc1ca5225c9a3fbcfc
Jonas Nyador <89290613+Ceteris90@users.noreply.github.com>
2022-09-17 00:29:11 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,8 +13,15 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-	{
-		putchar(low);
+	{	
+		if (low == 'z')
+		{
+			putchar(low );
+		}
+		else
+		{
+			putchar(low);
+		}
 	}
 
 	return (0);
