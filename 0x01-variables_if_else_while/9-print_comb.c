7875e6c02317a8091d84dcb5e74720bdd0249f59
Jonas Nyador <89290613+Ceteris90@users.noreply.github.com>
2022-09-17 01:37:51 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,16 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d <= 9; d++)
+	for (d = '0'; d <= '9'; d++)
+	{
 		putchar(d);
-		putchar(',');
-		putchar(' ');
-
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
+
 	return (0);
 }
