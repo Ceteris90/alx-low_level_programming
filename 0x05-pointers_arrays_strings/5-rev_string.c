9c05c70fc7f045cd41662fc9aadf9aba00dfd803
Jonas Nyador <89290613+Ceteris90@users.noreply.github.com>
2022-09-25 22:38:38 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,44 +1,35 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * _strlen - returns the length of a given string
- *
- *
- * @s: string to check the length of
- * Return: returns the length of the string
+ * rev_string - a function that reverse a string
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
  */
-
-int _strlen(char *s)
-{
-    int i;
-    
-    i = 0;
-    while (s[i] != '\0')
-        i++;
-    return (i);
-}
-
-
-/**
- * rev_string - reverses string in place, without printing it
- *
- * @s: string to reverse
- * Return: void
- */
-
 void rev_string(char *s)
 {
-    int i, len;
-    char a, z;
-    
-    len = _strlen(s) - 1;
-    i = 0;
-    while (i < len)
-    {
-        a = s[i];
-        z = s[len];
-        s[i++] = z;
-        s[len--] = a;
-    }
+	int len = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
+}
+#include "holberton.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: An integer input pointer
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
