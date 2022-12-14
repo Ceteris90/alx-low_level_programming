e21a2cb01964d74a0b25bf03ad7d429bbd56eb0e
ceteris90 <jonas.nyador@mathmods.eu>
2022-12-14 14:07:06 -0800

    15th Challenge

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -17,10 +17,9 @@ int main(void)
 	for(i = 2 ; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 49)
+		if (fibonacci[i]<=4000000)
 			printf("%ld\n", fibonacci[i]);
-		else
-			printf("%ld, ", fibonacci[i]);
+		
 	}	
 	return(0);
 }
