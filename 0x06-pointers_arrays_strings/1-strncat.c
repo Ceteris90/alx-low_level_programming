4fcfc4f995535679df57bed265ff46f9d65ad338
ceteris90 <jonas.nyador@mathmods.eu>
2022-12-22 17:34:40 -0800

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ *
+ *concatenate two strings
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != 0 && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (desk);
+}
