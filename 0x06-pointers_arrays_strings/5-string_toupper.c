5131be33d996c7df26e4cf331a57fd0b26f39ac6
ceteris90 <jonas.nyador@mathmods.eu>
2023-01-02 21:52:32 -0800

    5th Challenge

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,12 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (strc[i])
+	while (strc[i] != 0)
 	{
-		if (strc[i] >= 'a' && str[i] <= 'z')
+		if (strc[i] >= 97 && str[i] <= 122)
+		{
 			str[i] -= 32;
+		}
 		i++;
 	}
 	return(str);
