db4de86eadd07103734c202cc9970e918cc4d34e
ceteris90 <jonas.nyador@mathmods.eu>
2023-01-16 13:06:11 -0800

    Hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,6 @@ typedef struct dog
 	float age;
 	char *owner;
 }dog_t;
-
-/**
- * dog_t - Typedef for struct dog
- */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
