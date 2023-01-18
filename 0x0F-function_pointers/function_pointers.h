94276849f591c658da29145aa61a99b654397f50
ceteris90 <jonas.nyador@mathmods.eu>
2023-01-18 14:45:59 -0800

    Hot Fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,7 +8,7 @@
  * used in the 0x0E-function_pointers directory.
  */
 
-#include <stdlib.h>
+#include <stddef.h>
 int _putchar(char c)
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
