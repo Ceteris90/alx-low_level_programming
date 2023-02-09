9f66c2c0ae017f87dd117fe21cd4df9ab012ce98
Ceteris90 <jonas.nyador@mathmod.eu>
2023-02-09 15:29:50 -0800

    FILE I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,8 @@
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
- * @text_content: A pointer to a string to write to the file.
+ * Return: If the function fails or filename is NULL - -1.
+ * f the file does not exist the user lacks write permissions - -1.
  * Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
